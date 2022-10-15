cb338326bd8c6c2ecf9b1e1d012a226a353cf2e6
Keita <askeita@yahoo.com>
2022-10-15 22:54:18 +0300

    Initial commit

---
+++ src/main/java/com/example/hropenclass/HrOpenClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.hropenclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOpenClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrOpenClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hropenclass/HrOpenClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hropenclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOpenClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
